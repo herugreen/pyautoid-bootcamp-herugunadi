aa06dbf58e5492f2710269f88446c380d6772f97
Heru Gunadi <76834158+herugreen@users.noreply.github.com>
2021-09-17 11:32:32 +0700

    Update tugas01.py

--- tugas01.py
+++ tugas01.py
@@ -1,23 +1,20 @@
 from selenium import webdriver
-import time
 
-driver =  webdriver.Chrome(executable_path='C://chromedriver_win32/chromedriver.exe') 
+url_list = [
+            'https://www.noobtest.id',
+            'https://www.erzaf.com',
+            'https://www.orangsiber.com',
+            'https://www.demoqa.com',
+            'https://www.automatetheboringstuff.com'
+]
 
-driver.get('https://www.noobtest.id')
-driver.get('https://www.erzaf.com')
-driver.get('https://www.orangsiber.com')
-driver.get('https://www.demoqa.com')
-driver.get('https://www.automatetheboringstuff.com')
+driver = webdriver.Chrome()
+driver.minimize_window()
+
+for url in url_list:
+    driver.get(url)
+    Title = driver.title
+    Name = url.replace('https://','')
+    print(Name, '-', Title)
 
 driver.close()
-
-webtester = ('noobtest.id - Noob Test | A Platform for Noob Software Testers',
-            'erzaf.com - Erzaf - an IT Engginer',
-            'orangsiber.com - Home - Orang Siber',
-            'demoqa.com  ToolsQA',
-            'automatetheboringstuff.com - Automate the Boring Stuff with Python')
-
-time.sleep(2)
-
-for web in webtester:
-    print(web)
